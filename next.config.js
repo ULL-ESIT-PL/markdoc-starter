f5b81862cba661c65c95ddbec7315007294a2b58
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-12-25 12:11:27 +0000

    to make a build I have to remove all the js programs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withMarkdoc = require('@markdoc/next.js');
 
-module.exports =
-  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+let config = withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   });
+
+
+module.exports = config;
